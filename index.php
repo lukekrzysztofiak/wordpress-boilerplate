34e716e3fe45b3313eb7dd6a281785ea66a6148e
lukekrzysztofiak <87997353+lukekrzysztofiak@users.noreply.github.com>
2022-01-31 13:28:13 -0600

    Adding the loop
    WP basic loop

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-
+<?php get_header(); ?>
+<main id="x" role="x">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php get_template_part( 'x' ); ?>
+    <?php comments_template(); ?>
+    <?php endwhile; endif; ?>
+    <?php get_template_part( 'x', 'x' ); ?>
+</main>
+<?php get_footer(); ?>
