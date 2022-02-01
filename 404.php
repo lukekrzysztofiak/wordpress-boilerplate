8d0eeeb14abf73199e5cc778162d7bb9c41ea687
lukekrzysztofiak <87997353+lukekrzysztofiak@users.noreply.github.com>
2022-02-01 16:39:29 -0600

    404 Elements
    Added some error handling elements.

--- 404.php
+++ 404.php
@@ -4,6 +4,11 @@
   <div class="row">
     <div class="container">
       <!-- 404 Content - Add an engaging error message. -->
+      <h1 class="" itemprop="name"><?php esc_html_e( 'Not Found', 'lk_boilerplate' ); ?></h1>
+      <div class="" itemprop="mainContentOfPage">
+        <p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'lk_boilerplate' ); ?></p>
+      <?php get_search_form(); ?>
+      </div>
      </div>
   </div>
 </div>
