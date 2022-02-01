4d3155a8b1b5d57959b10a3adbfd0578423127c7
lukekrzysztofiak <87997353+lukekrzysztofiak@users.noreply.github.com>
2022-02-01 16:37:13 -0600

    Create 404.php
    Added a raw template for the error handling page.

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+
+<div class="container">
+  <div class="row">
+    <div class="container">
+      <!-- 404 Content - Add an engaging error message. -->
+     </div>
+  </div>
+</div>
+
+<?php get_footer() ?>
