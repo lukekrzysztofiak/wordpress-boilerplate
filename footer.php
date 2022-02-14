a08f0178d3a7e2f65528f37e71ca722e180bb46e
lukekrzysztofiak <87997353+lukekrzysztofiak@users.noreply.github.com>
2022-02-14 12:36:42 -0600

    Added Footer JavaScript
    Added footer JavaScript

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 <?php get_sidebar(); ?>
 </div>
-<footer id="footer" role="contentinfo">
-    <div id="copyright">
-        &copy;
-        <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?>
-        <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-    </div>
-</footer>
+      <footer class="pt-3 mt-4 text-muted border-top">
+        Copyright &copy;
+        <script>
+          var CurrentYear = new Date().getFullYear()
+            document.write(CurrentYear)
+        </script>
+        - Site Name | <a href="#">Privacy Policy</a>
+      </footer>
 </div>
 <?php wp_footer(); ?>
 </body>
